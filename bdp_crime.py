bc8866e254c99d43e4a6961ea57fe4d81b469f0f
JamesBenedict <jamesbenedict0@gmail.com>
2016-05-12 15:25:34 -0400

    removed github bs

--- bdp_crime.py
+++ bdp_crime.py
@@ -5,13 +5,14 @@ import urllib
 import bs4
 import requests
 import re
-
+# hey yaaaah
 new_dates = []
 site_links = []
 log_links = []
 
 history = open('data/past_dates.txt', 'r')
 past_dates = history.readlines()
+# print(past_dates)
 history.close()
 
 def url_grab():
@@ -19,54 +20,46 @@ def url_grab():
 	res = requests.get('http://www.indiana.edu/~iupd/dailyLog.html')
 	res.raise_for_status()
 	website = bs4.BeautifulSoup(res.text, 'html.parser')
-	# finds all likes with '.../Daily...' in them,
-	# then adds those to a list of links which I use to check if I have already anaylzed the pdfs
+	# .site_links = website.select('li > a')
 	for link in website.find_all('a', href=re.compile('Documents/Daily Log/')):
 		site_links.append(link)
+		# print(link)
 
 def history_check():
 	# checks to see if date was already anaylzed
 	for element in site_links:
-		# splits var into indexable thing
 		element_list = (str(element).split())
-		# extracting the section of the url with the date in it
 		date = element_list[2][4:11]
-		# single digit days messed my system up and I needed a workaround
 		if date[-1] == '.':
 			date = date[0:6]
+
 		date += '\n'
 
-		# stores new dates in var outside for loop, to later be added to my past dates. 
 		if date in past_dates:
 			pass
 		else: 
 			new_dates.append(date)
 
-	# takes all new dates from new_dates and adds to a text file. 
-	# This file is then used to make sure I don't duplicate a file I've already scraped
 	log = open('data/past_dates.txt', 'a')
 	log.writelines(new_dates)
 	log.close()
 	# print(new_dates)
 
-
 def scrapper():
-	# takes all the new dates I haven't scraped, and inserts that date into a url
 	for day in new_dates:
 		day = day.strip('\n')
 		input1 = 'http://www.indiana.edu/~iupd/Documents/Daily%20Log/'+day+'.pdf'
+		print(input1)
 		output = "data/"+day+'.pdf'
-		# Downloads the pdf and saves with date as name.
+		print(output)
 		testfile = urllib.URLopener()
 		testfile.retrieve(input1, output)
 
-#runs my three functions. I orginally had all of my scrapping as a class,
-#but then I wanted my date to be a universal 
 url_grab()
 history_check()
 scrapper()
 
-# strips line break from each day
+
 for day in new_dates:
 	day = day.strip('\n')
 	os.system("pdftotext '%s' '%s'" % ('data/'+day+'.pdf', 'data/'+day+'.txt'))
@@ -99,17 +92,28 @@ class Bdp_crime(object):
 		key_word_index = []
 		closest_index = []
 		temp_list = []
-		start_key_chunk = []
-		end_key_chunk = []
+		start_chunk = []
+		end_chunk = []
 		chunks = {}
+
 
 		#finding indexes of keywords in the crimelog text
 		#I use this to then find the closest start/end of a chunk 
 		#building the chunk middle out
+		# chunking
+		for i in range(len(lines)):
+			if '#:' in lines[i]:
+				# lines[i-1] = '-'*25
+				chunk_indexs.append(i)
+				# print(lines[i])
+				# print(i)
+				
+
+		# finding indexes of keywords
+# >>>>>>> parent of 46f7a56... started to clean up what i got
 		for i in range(len(lines)):
 			for word in key_words:
 				if word in lines[i]:
-					# Storing the location of every key crime we're interested in
 					key_word_index.append(i)
 					# print(word)
 					# print(key_word_index)
@@ -131,25 +135,24 @@ class Bdp_crime(object):
 					closest_index.append(item)
 			# I overstack exchanged here, and this is flattening my list of lists in a regular list.
 			temp_list.append(nsmallest(1, closest_index, key=lambda x: abs(x-num)))
-			# flattens my list of lists in a list
-			start_key_chunk = [val for sublist in temp_list for val in sublist]
+			start_chunk = [val for sublist in temp_list for val in sublist]
 		
 
 		# finds end of current crime chunk by searching for start of next chunk
-		for num in start_key_chunk:
+		for num in start_chunk:
 			for item in chunk_indexs:
 				if item > num:
-					end_key_chunk.append(item)
+					end_chunk.append(item)
 					break
-			# if the last crime chunk is a keyword, there is no '#:' to index after it, this uses the end of the document
-			#this is needed if the last crime of the page is a key crime
+			# if the last crime chunk is a keyword, there is no 'x:' index after it, this uses the end of the document
+			#this is for if the last crime of the page is a keyword crime
 			if num >= int(chunk_indexs[-1]):
-				end_key_chunk.append(len(lines))
+				end_chunk.append(len(lines))
 
 
 		# makes a dictonary with item = start, key = end
-		for i in range(len(start_key_chunk)):
-			chunks[start_key_chunk[i]] = end_key_chunk[i]
+		for i in range(len(start_chunk)):
+			chunks[start_chunk[i]] = end_chunk[i]
 
 		# appends every line between key and end to the crimes_list list
 		# The following is a long way of writing:
@@ -194,13 +197,23 @@ class Bdp_crime(object):
 
 
 	def crimes_all(self):
-		new_crimes = open('data/crime_new.txt', 'r')
-		new_content = new_crimes.readlines()
-		new_crimes.close()
+		# new_crimes = open('data/crime_new.txt', 'r')
+		# new_content = new_crimes.readlines()
+		# new_crimes.close()
 
-		log = open('data/crimes_all.txt', 'a')
-		log.writelines(' ')
-		log.close()
+		# log = open('data/crimes_all.txt', 'a')
+		# log.writelines(' ')
+		# log.close()
+		pass
+		# new_crimes = open('data/crime_new.txt', 'r')
+		# new_content = new_crimes.readlines()
+		# new_crimes.close()
+
+
+		# log = open('data/crimes_all.txt', 'a')
+		# log.writelines()
+		# log.close()
+# >>>>>>> parent of 46f7a56... started to clean up what i got
 
 	def cleanup(self):
 		pass

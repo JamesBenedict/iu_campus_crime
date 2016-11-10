d43427fd950775921e205c9886000d66c97f698f
jamesbenedict <jabenedi@indiana.edu>
2016-11-09 19:20:43 -0500

    prepping for gis day

---
+++ map/js/violent_bluelights.js
@@ -0,0 +1,82 @@
+// load map on bloomington
+var map = L.map('map').setView([39.166809, -86.521733], 14);
+
+// sets background tiles
+var CartoDB_Positron = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+	// subdomains: 'abcd',
+	maxZoom: 19
+}).addTo(map);
+
+var crimes = L.geoJson.ajax('../data/json/clean_crime.geojson', {
+
+ 
+   style: function(feature) {
+         if (feature.properties.crime_class == 'disorderly_conduct'){
+          return {color: '#C18BDA'};
+        } else if (feature.properties.crime_class == 'drug_sale'){
+          return {color: '#C18BDA'};
+        } else if (feature.properties.crime_class == 'DUI'){
+          return {color: '#C18BDA'};
+        } else {
+          return {color: '#bf6363'}
+        }
+
+    },
+
+    pointToLayer: function(feature, latlng) {
+        return new L.CircleMarker(latlng, {radius: 5, fillOpacity: 0.3, stroke: 0});
+    },
+
+    onEachFeature: function(feature, layer) {
+    // does this feature have a property named popupContent?
+     if (feature.properties && feature.properties.crime) {
+          layer.bindPopup(feature.properties.crime);
+      }
+    },
+
+     filter: function(feature){
+      if (feature.properties.crime_class == "DUI") return true
+      if (feature.properties.crime_class == "drug_sale") return true
+      if (feature.properties.crime_class == "disorderly_conduct") return true
+
+      if (feature.properties.crime_class == "assault") return true
+      if (feature.properties.crime_class == "missing_person") return true
+      // if (feature.properties.crime_class == "resisting_arrest") return true
+      if (feature.properties.crime_class == "robbery") return true
+      
+
+      if (feature.properties.crime_class == "sexual_misconduct") return true
+      if (feature.properties.crime_class == "domestic") return true
+      if (feature.properties.crime_class == "harrassment") return true
+      if (feature.properties.crime_class == "rape") return true
+      if (feature.properties.crime_class == "stalking") return true
+
+
+
+    }
+
+
+  
+});
+
+// map.addLayer(crimes);
+var bluelights = L.geoJson.ajax('../data/json/bluelights.geojson', {
+ style: function(feature) {
+//         if (feature.properties.crime_class == 'alcohol'){
+//           return {color: '#17655C'};
+      return {color: '#4b6892'};
+},
+ pointToLayer: function(feature, latlng) {
+        return new L.CircleMarker(latlng, {radius: 10, fillOpacity: 0.3, stroke: 0})
+       
+
+    },
+  
+
+});
+
+map.addLayer(bluelights);
+map.addLayer(crimes);
+
+
